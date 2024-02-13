d747fc35030728e44032cc1595bfd8384ff36384
Jocelyn McCain <jlmccain@wisc.edu>
2024-02-13 12:07:13 -0600

    bottles

--- bottles.py
+++ bottles.py
@@ -1,4 +1,8 @@
-print("99 bottles of beer on the wall")
-print("99 bottles of beer")
-print("Take one down, pass it around")
-print("98 bottles of beer on the wall")
+for i in range(99, 0, -1):
+    print(f"{i} bottles of beer on the wall")
+    print(f"{i} bottles of beer")
+    print("Take one down, pass it around")
+    if i > 1:
+        print(f"{i-1} bottles of beer on the wall\n")
+    else:
+        print("No more bottles of beer on the wall\n")
