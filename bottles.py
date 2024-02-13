869ad4c893265a1c33aede52710355e3c97cb5f3
Jocelyn McCain <jlmccain@wisc.edu>
2024-02-13 12:12:06 -0600

    bottles

--- bottles.py
+++ bottles.py
@@ -2,7 +2,10 @@ for i in range(99, 0, -1):
     print(f"{i} bottles of beer on the wall")
     print(f"{i} bottles of beer")
     print("Take one down, pass it around")
-    if i > 1:
+
+    if i == 6:
+        print(f"If one of those bottles should happen to fall, {i-1} bottles of beer on the wall...\n")
+    elif i > 1:
         print(f"{i-1} bottles of beer on the wall\n")
     else:
         print("No more bottles of beer on the wall\n")
