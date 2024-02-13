8240b544ad7f30e708cacad37d289b77e274f042
Jocelyn McCain <jlmccain@wisc.edu>
2024-02-13 12:22:56 -0600

    triangle

--- triangle.py
+++ triangle.py
@@ -1,5 +1,5 @@
-count = input("what size of triangle do you want? Enter a number. ")
+count = input("What size of triangle do you want? Enter a number: ")
 count2 = int(count)
+
 for num in range(1, count2 + 1):
     print("*" * num)
-print("")
