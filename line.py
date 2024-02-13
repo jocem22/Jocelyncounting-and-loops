2da4fe4f626bd58df2f348992bc776dcada7041a
Jocelyn McCain <jlmccain@wisc.edu>
2024-02-13 12:15:55 -0600

    line

--- line.py
+++ line.py
@@ -1,7 +1,6 @@
-count = input("How long of a line do you want? Enter a number. ")
+count = input("How long of a line do you want? Enter a number: ")
 
-for num in range(int(1)):
-    print("*", end=count)
+for num in range(int(count)):
+    print("*", end="")
+
 print("")
-  
-
